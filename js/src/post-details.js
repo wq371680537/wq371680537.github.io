448f44b148871dbf68a3fdf3deab1a90460e6a43
wq371680537 <371680537@qq.com>
2019-03-26 21:37:08 +0800

    Site updated: 2019-03-26 21:36:59

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -97,3 +97,11 @@ $(document).ready(function () {
       }) : NexT.utils.displaySidebar();
   }
 });
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    //Ĭ��������۵�
+    $("div.fold").css("display","none");
+});
